266aef3f8680b43c490fe82609657f7fb73d1945
Quejicus <jesusmugo91@gmail.com>
2024-06-20 20:38:44 +0200

    next steps

--- myapp/forms.py
+++ myapp/forms.py
@@ -2,7 +2,21 @@ from django import forms
 
 
 class CreateNewTask(forms.Form):
-    title = forms.CharField(label="Nombre de la tarea", max_length=200)
+    title = forms.CharField(
+        label="Nombre de la tarea",
+        max_length=200,
+        widget=forms.TextInput(attrs={"class": "input"}),
+    )
     description = forms.CharField(
-        label="Descripción de la tarea", widget=forms.Textarea, required=False
+        label="Descripción de la tarea",
+        widget=forms.Textarea(attrs={"class": "input"}),
+        required=False,
+    )
+
+
+class CreateNewProject(forms.Form):
+    name = forms.CharField(
+        label="Nombre del proyecto",
+        max_length=200,
+        widget=forms.TextInput(attrs={"class": "input"}),
     )
--- myapp/urls.py
+++ myapp/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
-    path("about/", views.about),
-    path("hello/<str:username>", views.hello_world),
-    path("projects/", views.projects),
-    path("tasks/", views.tasks),
-    path("create_task/", views.create_task),
+    path("", views.index, name="index"),
+    path("about/", views.about, name="about"),
+    path("hello/<str:username>", views.hello_world, name="hello"),
+    path("projects/", views.projects, name="projects"),
+    path("tasks/", views.tasks, name="tasks"),
+    path("create_task/", views.create_task, name="new_task"),
+    path("create_project/", views.create_project, name="new_project"),
+    path("projects/<int:id>", views.project_detail, name="project_detail"),
 ]
